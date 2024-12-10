7d4bb90609c9f8a3aaa731c1bd0824f5d06e0c66
RAKOVpy <dima.rakov.05@mail.ru>
2024-12-10 17:52:20 +0500

    design

--- Models_app/forms.py
+++ Models_app/forms.py
@@ -3,7 +3,8 @@ from django.core.exceptions import ValidationError
 
 
 class UploadForm(forms.Form):
-    dcm_file = forms.FileField(label='Select a DCM file', required=False)
+    dcm_file = forms.FileField(label='Select a DCM file', required=False,
+                               widget=forms.ClearableFileInput(attrs={'class': 'button-load'}))
 
     def clean_nifti_file(self):
         file = self.cleaned_data.get('dcm_file')
